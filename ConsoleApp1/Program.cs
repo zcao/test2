b4ef64dcc76355b7408553c85963b966c9832e42
Zhan Cao <zcao@mdsol.com>
2025-03-27 11:35:12 -0400

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using DynamicExpresso;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+var sample = new SampleClass();
+sample.SampleMethod();
+//var target = new Interpreter();
+//target.SetVariable("a", "1");
+// Create an interpreter instance
+var interpreter = new Interpreter();
+
+interpreter.SetFunction("pow", Math.Pow);
+
+var result = interpreter.Eval<double>("pow(2, 3)"); // 2^3 = 8
+var a = Regex.Replace("pow[1,2]", @"(\w+)\[(.*?)\]", "$1($2)");
+Console.WriteLine(result);
+
+public class SampleClass
+{
+
+    public void SampleMethod()
+    {
+        var a = new SampleClass();
+        a.NotNull();
+        a.Test("b");
+        Console.WriteLine("This line will never be executed.");
+    }
+
+    public void Test(string a, [CallerArgumentExpression("a")]
+        string argumentExpression = null)
+    {
+
+    }
+}
+
+
+public static class Ext
+{
+  
+    public static T NotNull<T>(
+   
+        this T obj,
+    string message = null,
+    [CallerArgumentExpression("obj")]
+        string argumentExpression = null)
+    where T : class
+    {
+        if (obj == null)
+        {
+            throw new ArgumentException(
+                message ?? $"Expected object of type '{typeof(T).FullName}' to be not null",
+                message == null ? argumentExpression : null);
+        }
+
+        return obj;
+    }
+}
---
+++ build/Build.cs
@@ -0,0 +1,98 @@
+using System;
+using System.IO.Compression;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities.Collections;
+using Nuke.Common.Tools.MSBuild;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using System.IO;
+using Nuke.Common.Tools.MinVer;
+using Nuke.Common.Tools.DotNet;
+using System.Diagnostics;
+
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration;
+
+    [Solution(GenerateProjects = true)]
+
+    readonly Solution Solution;
+
+    AzurePipelines AzurePipelines => AzurePipelines.Instance;
+
+    readonly MinVer MinVer;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            Console.WriteLine("Cleaning...");
+            DotNetTasks.DotNetClean();
+        });
+
+    Target Restore => _ => _
+    // .DependsOn(Clean)
+        .Executes(() =>
+        {
+            // Debugger.Launch();
+            Console.WriteLine("Restoring...");
+            DotNetTasks.DotNetRestore();
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {  
+             Console.WriteLine($"Building...{Solution.Name} in {RootDirectory} {Configuration}MinVer{MinVer}");
+
+            DotNetTasks.DotNetBuild();
+            MSBuildTasks.MSBuild(s => s
+                .SetTargetPath(Solution)
+                .SetConfiguration(Configuration.Debug));
+         
+        });
+    Target Test => _ => _
+        // .DependsOn(Compile)
+        .Executes(() =>
+        {
+            Console.WriteLine("Running tests...");
+            DotNetTasks.DotNetTest(s => s
+                .SetProjectFile("AAAA")
+                .SetFilter("TestCategory=PB_TEST")
+                .SetLoggers("nunit;LogFilePath=../_Reports/results.xml")
+                .SetConfiguration(Configuration.Debug));
+        });
+        
+    Target Publish => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            Console.WriteLine($"Building...{Solution.Name} in {RootDirectory} {Configuration} MinVer{MinVer}");
+            var debugFolder = RootDirectory/"ConsoleApp1" / "bin" / "Debug";
+            var zipFile = RootDirectory / "debug.zip";
+            if (File.Exists(zipFile))
+            {
+                File.Delete(zipFile);
+            }
+            ZipFile.CreateFromDirectory(debugFolder, zipFile);
+        
+        });
+
+}
