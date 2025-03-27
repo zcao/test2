055234ef66e79e271c08b27fec0236d9218a9516
Zhan Cao <zcao@mdsol.com>
2025-03-27 11:47:42 -0400

    sh

--- build/Build.cs
+++ build/Build.cs
@@ -18,7 +18,9 @@ using Nuke.Common.Tools.MinVer;
 using Nuke.Common.Tools.DotNet;
 using System.Diagnostics;
 using Nuke.Common.CI.GitHubActions;
-[GitHubActions("test1", GitHubActionsImage.UbuntuLatest, On = new[] { GitHubActionsTrigger.Push }, InvokedTargets = new[] { nameof(Test) })]
+[GitHubActions("test1", GitHubActionsImage.UbuntuLatest, 
+On = new[] { GitHubActionsTrigger.Push }, 
+InvokedTargets = new[] { nameof(Test) })]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
